7b54e6a3a658f56210fd2509c750f2ebadfe324d
wu <timmy61501@gmail.com>
2024-01-11 10:56:04 +0800

    製作後台管理登入功能

--- back.php
+++ back.php
@@ -1,3 +1,14 @@
+<?php include_once "./api/db.php";
+
+if (!empty($_POST)) {
+  if ($_POST['acc'] == 'admin' && $_POST['pw'] == '1234') {
+    $_SESSION['login'] = 1;
+  } else {
+    $error = "<div class='ct' style='color:red;'>帳號或密碼錯誤</div>";
+  }
+}
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0055)?do=admin -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -22,24 +33,52 @@
       </marquee>
     </div>
     <div id="mm">
-      <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
-        <a href="?do=tit">網站標題管理</a>|
-        <a href="?do=go">動態文字管理</a>|
-        <a href="?do=poster">預告片海報管理</a>|
-        <a href="?do=movie">院線片管理</a>|
-        <a href="?do=order">電影訂票管理</a>
-      </div>
-      <div class="rb tab">
-        <?php
-        $do = $_GET['do'] ?? 'main';
-        $file = "./back/{$do}.php";
-        if (file_exists($file)) {
-          include $file;
-        } else {
-          include "./back/main.php";
-        }
-        ?>
-      </div>
+      <?php
+      if (isset($_SESSION['login'])) {
+      ?>
+        <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
+          <a href="?do=tit">網站標題管理</a>|
+          <a href="?do=go">動態文字管理</a>|
+          <a href="?do=poster">預告片海報管理</a>|
+          <a href="?do=movie">院線片管理</a>|
+          <a href="?do=order">電影訂票管理</a>
+        </div>
+        <div class="rb tab">
+          <?php
+          $do = $_GET['do'] ?? 'main';
+          $file = "./back/{$do}.php";
+          if (file_exists($file)) {
+            include $file;
+          } else {
+            include "./back/main.php";
+          }
+          ?>
+        </div>
+      <?php
+      } else {
+      ?>
+        <form action="?" method="post" style="width:35%;margin:20px auto;">
+          <h3 class='ct'>管理者登入</h3>
+          <?php
+          if (isset($error)) {
+            echo $error;
+          }
+          ?>
+          <table>
+            <tr>
+              <td>帳號:</td>
+              <td><input type="text" name="acc"></td>
+            </tr>
+            <tr>
+              <td>密碼:</td>
+              <td><input type="password" name="pw"></td>
+            </tr>
+          </table>
+          <div class="ct"><input type="submit" value="登入"></div>
+        </form>
+      <?php
+      }
+      ?>
     </div>
     <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
   </div>
