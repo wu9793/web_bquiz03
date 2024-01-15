f5846f1e34742ff35f5b53dc1d127bc4373649a3
wu <timmy61501@gmail.com>
2024-01-15 14:47:07 +0800

    依據當下時間計算可訂的場次

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -1,8 +1,10 @@
 <?php include_once 'db.php';
 $movie = $_GET['movie'];
 $date=$_GET['date'];
+$H=date("G");
+$start=($H<14)?1:7-ceil((24-$H)/2);
 
-for($i=1;$i<=5;$i++){
+for($i=$start;$i<=5;$i++){
     echo "<option value='{$sess[$i]}'>{$sess[$i]} 剩餘座位 20</option>";
 }
 
