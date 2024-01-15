0d54a0cea37d91a3bc91af30cc7fd0802dd93f01
wu <timmy61501@gmail.com>
2024-01-15 14:01:03 +0800

    簡化後端計算剩餘上映日期的計算方式

--- api/get_dates.php
+++ api/get_dates.php
@@ -1,11 +1,10 @@
 <?php include_once 'db.php';
 $movie = $_GET['id'];
-$ondate = $Movie->find($movie)['ondate'];
-$today = date("Y-m-d");
-for ($i = 0; $i < 3; $i++) {
-    $date = strtotime("+$i days", strtotime($ondate));
-    if ($date >= strtotime($today)) {
-        $str = date("Y-m-d", $date);
-        echo "<option value='{$str}'>$str</option>";
-    }
+$ondate = strtotime($Movie->find($movie)['ondate']);
+$end=strtotime("+2 days",$ondate);
+$today = strtotime(date("Y-m-d"));
+$diff=($end-$today)/(60*60*24);
+for ($i = 0; $i <=$diff; $i++) {
+    $date = date("Y-m-d", strtotime("+$i days"));
+    echo "<option value='$date'>$date</option>";
 }
