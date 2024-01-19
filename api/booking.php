31d77d080023165b2f91f298697a7182ba891f5a
wu <timmy61501@gmail.com>
2024-01-19 14:07:39 +0800

    完成已訂位紀錄顯示不同icon

--- api/booking.php
+++ api/booking.php
@@ -4,6 +4,15 @@ $movie = $Movie->find($_GET['movie_id']);
 $date = $_GET['date'];
 $session = $_GET['session'];
 
+$ords=$Order->all(['movie'=>$movie['name'],
+                    'date'=>$date,
+                    'session'=>$session]);
+$seats=[];
+foreach($ords as $ord){
+    $tmp=unserialize($ord['seats']);
+    $seats=array_merge($seats,$tmp);
+}
+
 ?>
 <style>
     #room {
@@ -44,9 +53,15 @@ $session = $_GET['session'];
             echo (($i % 5) + 1) . "號";
             echo "</div>";
             echo "<div class='ct'>";
-            echo "<img src='./icon/03D02.png'>";
+            if(in_array($i,$seats)){
+                echo "<img src='./icon/03D03.png'>";
+            }else{
+                echo "<img src='./icon/03D02.png'>";
+            }
             echo "</div>";
-            echo "<input class='chk' type='checkbox' name='chk'>";
+            if(!in_array($i,$seats)){
+                echo "<input class='chk' type='checkbox' value='$i' name='chk'>";
+            }
             echo "</div>";
         };
         ?>
